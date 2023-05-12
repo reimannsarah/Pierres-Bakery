8de19f12f7db834891e589604cd77e917e8a2fd5
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-12 09:21:57 -0700

    make all properties public

--- Bakery/Models/AllVendors.cs
+++ Bakery/Models/AllVendors.cs
@@ -4,6 +4,10 @@ namespace Bakery.Models
 {
   public abstract class AllVendors
   {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order> {};
     public static List<Vendor> VendorList { get; set; } = new List<Vendor> {};
   }
 }

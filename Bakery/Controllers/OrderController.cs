23a9f289af88f252913663fef22b22737d9a76be
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-13 14:39:20 -0700

    add path to grab date from order form and create Order

--- Bakery/Controllers/OrderController.cs
+++ Bakery/Controllers/OrderController.cs
@@ -16,5 +16,25 @@ namespace Bakery.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult New(int id)
+    {
+      return View(id);
+    }
+    [HttpPost("/vendors/{id}/order")]
+    public ActionResult Create(int id, string orderInputVendorName, string[] type, int[] amount )
+    {
+      Dictionary<string,object>model = new Dictionary<string,object>();
+      Order newOrder = new Order(orderInputVendorName, type, amount);
+      newOrder.CalculateTotal();
+      Vendor selectedVendor = AllVendors.Find(id);
+      selectedVendor.AddOrder(newOrder);
+      Dictionary<string,int> cart = newOrder.Cart;
+      model.Add("order",newOrder);
+      model.Add("cart", cart);
+      return View(model);
+
+    }
   }
 }

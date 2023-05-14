f98c45ca9a034abf1e04f0c736277b395e03c563
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-13 17:18:03 -0700

    update names of menu items to match values in form

--- Bakery/Models/Menu.cs
+++ Bakery/Models/Menu.cs
@@ -9,13 +9,11 @@ namespace Bakery.Models
       {"sourdough", 2},
       {"baguette", 3},
       {"rye", 5},
-      {"whole wheat", 4},
+      {"whole-wheat", 4},
       {"pumpernickle", 5},
       {"blueberry muffin", 2},
-      {"branmuffin", 2},
+      {"bran-muffin", 2},
       {"criossant", 3},
-      {"marionberry scone", 3},
-      
     };
   }
 }
